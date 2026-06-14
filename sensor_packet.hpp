f7b092a537a7daf99868f5700f7b42bf7cdf942b
GAITH SLMAN <gaithslman7@gmail.com>
2026-06-14 14:49:35 +0300

    Combine accel_x, accel_y, and accel_z into one line

--- sensor_packet.hpp
+++ sensor_packet.hpp
@@ -2,11 +2,10 @@
 #include <cstdint>
 
 struct alignas(4) SensorPacket {
-    float    accel_x;
-    float    accel_y;
-    float    accel_z;
+    float    accel_x, accel_y, accel_z;
     uint32_t heart_rate;
     uint64_t timestamp_us;
 };
+
 
 static_assert(sizeof(SensorPacket) == 24);
